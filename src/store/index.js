7b0cfb1848cf162a4da3611eb8b0a6509c3cd6f2
tenohira <67691176+gyuugoro@users.noreply.github.com>
2021-01-31 10:12:56 +0900

    いかさま追加

--- src/store/index.js
+++ src/store/index.js
@@ -29,53 +29,61 @@ export default new Vuex.Store({
   actions: {
 
     character({ state }) {
-      const random = Math.floor(Math.random() * 110) + 1
 
-      if (random >= 1 && random < 31) {
-        state.character = 'メロス';
-        state.level = '低'
-      } else if (random >= 31 && random < 61) {
-        state.character = 'セリヌンティウス';
-        state.level = '低'
-      } else if (random >= 61 && random < 91) {
-        state.character = 'ディオニス';
-        state.level = '低'
-      } else if (random >= 91 && random < 96) {
-        state.character = 'メロスの妹';
-        state.level = '中'
-      } else if (random >= 96 && random < 101) {
-        state.character = "フィロストラトス";
-        state.level = '中'
-      } else if (random == 101) {
-        state.character = "山賊";
+
+
+      if (state.key == "れおと") {
+        state.character = '永遠の裏切者、地上で最も不名誉の人種';
         state.level = '高'
-      } else if (random == 102) {
-        state.character = "メロスの妹の結婚相手";
-        state.level = '高'
-      } else if (random == 103) {
-        state.character = "メロスに揺さぶられた老爺";
-        state.level = '高'
-      } else if (random == 104) {
-        state.character = "メロスに緋のマントを捧げた少女";
-        state.level = '高'
-      } else if (random == 105) {
-        state.character = "ゼウス";
-        state.level = '高'
-      } else if (random == 106) {
-        state.character = "メロスに蹴とばされた犬";
-        state.level = '高'
-      } else if (random == 107) {
-        state.character = "メロスの羊";
-        state.level = '高'
-      } else if (random == 108) {
-        state.character = "太宰治";
-        state.level = '高'
-      } else if (random == 109) {
-        state.character = "永遠の裏切者、地上で最も不名誉の人種";
-        state.level = '高'
-      } else if (random == 110) {
-        state.character = "王に殺された賢臣のアレキス";
-        state.level = '高'
+      } else {
+        const random = Math.floor(Math.random() * 110) + 1
+
+        if (random >= 1 && random < 31) {
+          state.character = 'メロス';
+          state.level = '低'
+        } else if (random >= 31 && random < 61) {
+          state.character = 'セリヌンティウス';
+          state.level = '低'
+        } else if (random >= 61 && random < 91) {
+          state.character = 'ディオニス';
+          state.level = '低'
+        } else if (random >= 91 && random < 96) {
+          state.character = 'メロスの妹';
+          state.level = '中'
+        } else if (random >= 96 && random < 101) {
+          state.character = "フィロストラトス";
+          state.level = '中'
+        } else if (random == 101) {
+          state.character = "山賊";
+          state.level = '高'
+        } else if (random == 102) {
+          state.character = "メロスの妹の結婚相手";
+          state.level = '高'
+        } else if (random == 103) {
+          state.character = "メロスに揺さぶられた老爺";
+          state.level = '高'
+        } else if (random == 104) {
+          state.character = "メロスに緋のマントを捧げた少女";
+          state.level = '高'
+        } else if (random == 105) {
+          state.character = "ゼウス";
+          state.level = '高'
+        } else if (random == 106) {
+          state.character = "メロスに蹴とばされた犬";
+          state.level = '高'
+        } else if (random == 107) {
+          state.character = "メロスの羊";
+          state.level = '高'
+        } else if (random == 108) {
+          state.character = "太宰治";
+          state.level = '高'
+        } else if (random == 109) {
+          state.character = "永遠の裏切者、地上で最も不名誉の人種";
+          state.level = '高'
+        } else if (random == 110) {
+          state.character = "王に殺された賢臣のアレキス";
+          state.level = '高'
+        }
       }
     },
     getDatas({ commit }) {
@@ -84,7 +92,8 @@ export default new Vuex.Store({
       firebase
         .firestore()
         .collection('meros-game')
-        .get('time')
+        .orderBy('time', 'asc')
+        .get()
         .then((docs) => {
           docs.forEach((doc) => {
             commit('addDatas', { name: doc.data().name, character: doc.data().character })
@@ -92,6 +101,11 @@ export default new Vuex.Store({
         })
     },
     setDatas({ state, dispatch }) {
+      if (state.key == "テスト") {
+        dispatch('getDatas')
+        return;
+      }
+
       firebase.firestore().collection('meros-game').doc(state.key).set({
         character: state.character,
         time: new Date().getTime(),
@@ -99,6 +113,7 @@ export default new Vuex.Store({
       }).then(() => {
         dispatch('getDatas')
       })
+
     }
   },
 })
